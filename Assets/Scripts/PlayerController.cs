237973eea1fe97cfaa2f10761a22c8acab62223b
zqh0429 <1627212412@qq.com>
2023-06-17 15:15:29 +0800

    06/17/15:15

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class PlayerController : MonoBehaviour
 {
-   void Update()
+    public float jumpForce = 5f;
+    private bool canJump = true;
+
+    private void Update()
     {
-        OnMouseDrag();
+        if (Input.GetMouseButtonDown(0) && canJump)
+        {
+            Jump();
+        }
     }
 
-    private void OnMouseDrag()
+    private void Jump()
     {
-        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pos.z);
-        transform.position = Camera.main.ScreenToWorldPoint(MousePos);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f); // 确保Y轴速度为0，防止连续跳跃叠加
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true; // 落地后可以再次跳跃
+        }
     }
 }
+
